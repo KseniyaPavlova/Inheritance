a9098f68e9bc1edc8caa89dec61538d4d12da34a
KseniyaPavlova <101828897+KseniyaPavlova@users.noreply.github.com>
2022-06-08 00:33:27 +0300

    Academy

---
+++ Academy/Source.cpp
@@ -0,0 +1,231 @@
+#include<iostream>
+#include<string>
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Human
+{
+protected:
+	std::string last_name;
+	std::string first_name;
+	unsigned int age;
+public:
+	const std::string& get_last_name()const
+	{
+		return last_name;
+	}
+	const std::string& get_first_name()const
+	{
+		return first_name;
+	}
+	unsigned int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(const std::string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const std::string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(unsigned int age)
+	{
+		this->age = age;
+	}
+
+	//					Consnructors:
+	Human(const std::string last_name, const std::string& first_name, unsigned int age)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "HConstructor:\t" << this << endl;
+	}
+	~Human()
+	{
+		cout << "HDestructor:\t" << this << endl;
+	}
+
+	//					Methods:
+	void print()const
+	{
+		cout << last_name << " " << first_name << " " << age << " years.\n";
+	}
+};
+
+class Student :public Human
+{
+	std::string speciality;
+	std::string group;
+	unsigned int year;
+	double rating;
+	double attendance;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	const std::string& get_group()const
+	{
+		return group;
+	}
+	unsigned int get_year()const
+	{
+		return year;
+	}
+	double get_rating()const
+	{
+		return rating;
+	}
+	double get_attendance()const
+	{
+		return attendance;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_group(const std::string& group)
+	{
+		this->group = group;
+	}
+	void set_year(unsigned int year)
+	{
+		this->year = year;
+	}
+	void set_rating(double rating)
+	{
+		this->rating = rating;
+	}
+	void set_attendance(double attendance)
+	{
+		this->attendance = attendance;
+	}
+	//			Constructors:
+	Student
+	(
+		const std::string& last_name, const std::string& first_name,unsigned int age,
+		const std::string& speciality, const std::string& group, unsigned int year, double rating, double attendance
+	)	:Human(last_name, first_name, age)
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_year(year);
+		set_rating(rating);
+		set_attendance(attendance);
+		cout << "SConstructor:\t" << this << endl;
+	}
+	~Student()
+	{
+		cout << "SDestructor:\t" << this << endl;
+	}
+	//					Methods:
+	void print()const
+	{
+		Human::print();
+		cout <<"Специальность: "<< speciality + " " +"Группа: " +group << " " << "Курс: " << year << " " << "Рейтинг: " << rating << " " << "Посещаемость: " << attendance << endl;
+	}
+};
+
+class Teacher :public Human
+{
+	std::string speciality;
+	unsigned int experience;
+public:
+	const std::string& get_speciality()const
+	{
+		return speciality;
+	}
+	unsigned int get_experience()const
+	{
+		return experience;
+	}
+	void set_speciality(const std::string& speciality)
+	{
+		this->speciality = speciality;
+	}
+	void set_experience(unsigned int experience)
+	{
+		this->experience = experience;
+	}
+	
+	//			Constructors:
+	Teacher
+	(
+		const std::string& last_name, const std::string& first_name, unsigned int age,
+		const std::string& speciality, unsigned int experience
+	) :Human(last_name, first_name, age)
+	{
+		set_speciality(speciality);
+		set_experience(experience);
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Teacher()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+	//					Methods:
+	void print()const
+	{
+		Human::print();
+		cout << "Специальность: " << speciality + " " << "Опыт: " << experience << endl;
+	}
+};
+class Graduate :public Student
+{
+	std::string diplom;
+public:
+	const std::string& get_diplom()const
+	{
+		return diplom;
+	}
+	void set_diplom(const std::string& diplom)
+	{
+		this->diplom = diplom;
+	}
+
+	//			Constructors:
+	Graduate
+	(
+		const std::string& last_name, const std::string& first_name, unsigned int age,
+		const std::string& speciality, const std::string& group, unsigned int year, double rating, double attendance,
+		const std::string& diplom
+	) :Student
+	(
+		last_name, first_name, age,
+		speciality, group,year, rating,attendance
+	)
+	{
+		set_diplom(diplom);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+	//					Methods:
+	void print()const
+	{
+		Student::print();
+		cout << "Тема диплома: " << diplom << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	Human human("Montana", "Antonio", 25);
+	human.print();
+
+	Student pinkman("Pinkman", "Jessie", 23, "Chemistry", "WW_220", 1, 90, 85);
+	pinkman.print();
+	
+	Teacher teacher("Ivanov", "Ivan", 43, "Chemistry", 20);
+	teacher.print();
+
+	Graduate graduate("Sidorov", "Alexey", 22, "Programming", "WW_420", 4, 90, 45, "С++");
+	graduate.print();
+}
