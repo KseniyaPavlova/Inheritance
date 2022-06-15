65649a3219441f11ad9fa17ff0ba27bfb26d034a
KseniyaPavlova <101828897+KseniyaPavlova@users.noreply.github.com>
2022-06-15 22:16:46 +0300

    Human & Student DONE; TODO: Teacher&Graduate;

--- Files/Source.cpp
+++ Files/Source.cpp
@@ -1,4 +1,5 @@
-﻿#include<iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
 #include<fstream>
 using std::cin;
 using std::cout;
@@ -7,11 +8,41 @@ using std::endl;
 void main()
 {
 	setlocale(LC_ALL, "");
+	const int SIZE = 256;
+	char sz_filename[SIZE] = {}; //sz_ - String Zero
+	cout << "Введите имя файла: ";
+	cin.getline(sz_filename, SIZE);
+	if (strcmp(sz_filename + strlen(sz_filename) - 4, ".txt"))
+		strcat(sz_filename, ".txt");
 	std::ofstream fout;		//1) Создаем поток
-	fout.open("File.txt", std::ios_base::app);	//2) Открываем поток
-	fout << "Hello World!\n"; //3) Выводим поток
+	fout.open(sz_filename, std::ios_base::app);	//2) Открываем поток
+	char sz_contents[SIZE] = {};
+	cout << "Введите содержимое файла: ";
+	cin.getline(sz_contents, SIZE);
+	fout << sz_contents; //3) Выводим поток
 	fout.close();			//4) Закрываем поток
 	//Потоки обязательно нужно закрывать, это также важно, как удалять память
 	//Сколько раз был вызван метод open(), столько же раз нужно вызвать метод close();
-	system("notepad File.txt");
+	char sz_command[SIZE] = "notepad ";
+	strcat(sz_command, sz_filename);
+	system(sz_command);
+
+
+
+	//	std::ofstream fout;		//1) Создаем поток
+	//	char name;
+	//	cout << "Введите имя файла"; cin >> name >> ".txt";
+	//	fout.open(name, std::ios_base::app);	//2) Открываем поток
+	//	fout << "Hello World!\n"; //3) Выводим поток
+	//	fout.close();			//4) Закрываем поток
+	//	//system("notepad File.txt");
+	//
+	////const int size = 256;
+	////	char name[size] = {};
+	////	cout << "Введите имя файла"; cin >> name >> ".txt";
+	////	std::ofstream fout;
+	////	fout.open(name, std::ios_base::app);
+	////	fout << "Hello World!\n";
+	////	fout.close();
+	////	system("notepad File.txt");
 }
